ded045ebde50fb273f216547f198b38b3891dde0
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-24 18:06:20 -0300

    Adiciona o a pagina Configuracoes para o requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import Login from './pages/Login';
+import Configuracoes from './pages/Configuracoes';
 import './App.css';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <Switch>
+          <Route path="/configuracoes" component={ Configuracoes } />
           <Route path="/" component={ Login } />
         </Switch>
       </header>
