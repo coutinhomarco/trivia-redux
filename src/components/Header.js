1d9468c36ded4b666ba46488e72a942f015a30ef
Marco <marquinco@outlook.com>
2021-11-25 18:50:05 -0300

    Cumpre requisito 11 parcialmente

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
+import { sendUserInfo } from '../redux/actions';
 
 class Header extends Component {
-  render() {
-    const { email, name } = this.props;
+  constructor(props) {
+    super(props);
+    const { email } = this.props;
     const hashGerada = md5(email).toString();
+    this.state = ({
+      gravatarLink: `https://www.gravatar.com/avatar/${hashGerada}`,
+    });
+  }
+
+  componentDidMount() {
+    const { dispatchGravatar } = this.props;
+    const { gravatarLink } = this.state;
+    dispatchGravatar({ gravatar: gravatarLink });
+  }
+
+  render() {
+    const { name } = this.props;
+    const { gravatarLink } = this.state;
     return (
       <header>
         <img
-          src={ `https://www.gravatar.com/avatar/${hashGerada}` }
+          src={ gravatarLink }
           data-testid="header-profile-picture"
           alt="Avatar"
         />
@@ -26,9 +42,14 @@ const mapStateToProps = (state) => ({
   name: state.login.name,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  dispatchGravatar: (state) => dispatch(sendUserInfo(state)),
+});
+
 Header.propTypes = {
+  dispatchGravatar: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
