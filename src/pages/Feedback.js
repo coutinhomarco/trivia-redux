7f746f87b41b1ba0a6798cb4cc5f10d3155b8b6c
Gracio <gustavogracio@hotmail.com>
2021-11-26 19:09:08 -0300

    Requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      score: 0,
+      gravatarEmail: '',
+    }
+  }
+  componentDidMount() {
+    const { player } = JSON.parse(localStorage.getItem('state'));
+    this.setState(player);
+  }
   render() {
+    const { gravatarEmail, name, score } = this.state;
     return (
-      <div>
+      <header>
         <p data-testid="feedback-text">Tente novamente</p>
-      </div>
+        <img 
+          src={ `https://www.gravatar.com/avatar/${gravatarEmail}` } 
+          data-testid="header-profile-picture" 
+          alt="Gravatar"
+        />
+        <p data-testid="header-player-name">{name}</p>
+        <p data-testid="header-score">{score}</p>
+      </header>
     );
   }
 }
+
 
 export default Feedback;
