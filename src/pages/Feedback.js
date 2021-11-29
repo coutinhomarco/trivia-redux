37be60e839399aa160cbeaf1d59bb7fcc0361695
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-29 15:47:52 -0300

    Requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -9,6 +9,7 @@ class Feedback extends Component {
       gravatarEmail: '',
     };
     this.setPlayerState = this.setPlayerState.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
   }
 
   componentDidMount() {
@@ -20,19 +21,35 @@ class Feedback extends Component {
     this.setState(player);
   }
 
+  renderMessage() {
+    const { assertions } = this.state;
+    const MINIMAL_ASSERTIONS = 3;
+    if (assertions >= MINIMAL_ASSERTIONS) {
+      return (
+        <h1 data-testid="feedback-text">Mandou bem!</h1>
+      );
+    }
+    return (
+      <h1 data-testid="feedback-text">Podia ser melhor...</h1>
+    );
+  }
+
   render() {
     const { gravatarEmail, name, score } = this.state;
     return (
-      <header>
-        <p data-testid="feedback-text">Tente novamente</p>
-        <img
-          src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
-          data-testid="header-profile-picture"
-          alt="Gravatar"
-        />
-        <p data-testid="header-player-name">{name}</p>
-        <p data-testid="header-score">{score}</p>
-      </header>
+      <>
+        <header>
+          <p data-testid="feedback-text">Tente novamente</p>
+          <img
+            src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
+            data-testid="header-profile-picture"
+            alt="Gravatar"
+          />
+          <p data-testid="header-player-name">{name}</p>
+          <p data-testid="header-score">{score}</p>
+        </header>
+        { this.renderMessage()}
+      </>
     );
   }
 }
